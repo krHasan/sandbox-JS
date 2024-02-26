80d1eefa63323f5af5fb2fa1c671cc203e8d0d29
Rakib <khandokerrakib.hasan@bracits.com>
2024-02-27 01:25:39 +0600

    feat(learnOne): add new content in leanOne page and jquery

--- index.js
+++ index.js
@@ -11,18 +11,20 @@ $(document).ready(function () {
             text = text.replace(new RegExp("\\bfunction\\b", "gi"), '<span style="color: #045694; font-weight: bold">$&</span>');
 
             // #015493 light blue
-            let lightBlueArray = ['return', 'if', 'else', 'const', 'let', 'var', 'while', 'new', 'async',
+            let lightBlueArray = ['false', 'true', 'return', 'if', 'else', 'const', 'let', 'var', 'while', 'new', 'async',
                 'try', 'await', 'catch', 'success', 'data', 'type', 'dataType', 'name', 'id', 'document', 'undefined',
-                'null', 'class', 'table', 'label', 'input', 'tmpl', 'div'];
+                'null', 'class', 'table', 'label', 'input', 'tmpl', 'div', 'DOMContentLoaded', 'location'];
             lightBlueArray.forEach(element => {
                 text = text.replace(new RegExp("\\b" + element + "\\b", "gi"), '<span style="color: #015493">$&</span>');
             });
 
             // #C16D27 orange
-            let orangeArray = ['false', 'true', 'require', 'Promise', 'setTimeout', 'resolve', 'get', 'JSON',
+            let orangeArray = ['require', 'Promise', 'setTimeout', 'resolve', 'get', 'JSON',
                 'stringify', 'log', 'ajax', 'then', 'XMLHttpRequest', 'done', 'catch', 'toLocaleString',
                 'fail', 'val', 'serialize', 'reload', 'jqXHR', 'errorThrown', 'slice', 'this', 'prop',
-                'attr', 'removeAttr', 'change', 'ready', 'blur', 'toFixed', 'parseFloat', 'inputFilter', 'test'];
+                'attr', 'removeAttr', 'change', 'ready', 'blur', 'toFixed', 'parseFloat', 'inputFilter', 'test',
+                'getElementById', 'addEventListener', 'createElement', 'appendChild', 'contains', 'valueAsNumber',
+                'replace', 'split'];
             orangeArray.forEach(element => {
                 text = text.replace(new RegExp("\\b" + element + "\\b", "gi"), '<span style="color: #C16D27">$&</span>');
             })
